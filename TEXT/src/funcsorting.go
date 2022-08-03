4ab0e23a1ebb82dd1782864d87904b9ca95fd56e
anyuquan <anyuquan@bytedance.com>
2022-08-03 15:29:57 +0800

    jj'
    Change-Id: I135d40c7c122928f301db4c577583164d6b35f1f

--- TEXT/src/funcsorting.go
+++ TEXT/src/funcsorting.go
@@ -1,11 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 // 自定义排序需要重写类型和swap 方法和less方法
 type byLength []string
 
 func (s byLength) Len() int {
-
+	return len(s)
+}
+func (s byLength) Swap(i int, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s byLength) Less(i int, j int) bool {
+	return len(s[i]) < len(s[j])
 }
 func main() {
-
+	fruit := []string{"aannnnnna", "bbbbb", "xccccccc"}
+	sort.Sort(byLength(fruit))
+	fmt.Println(fruit)
 }
